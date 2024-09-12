a331b725f759ce49abe45adff66911e8e939b835
Diomande Dro Freddy Junior <frediustcdev@gmail.com>
2024-09-12 13:43:22 +0000

    first api

---
+++ app/(api)/user/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export const GET = async () => {
+  const id = Math.round(Math.random() * 10);
+
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  const user = await response.json();
+
+  return NextResponse.json({ user, date: new Date().toISOString() });
+};
